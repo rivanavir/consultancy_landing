f7233292390c3a51981fdbc62c3c1a88dd836fb9
Ivan <Ivan@Mac-mini-IvanO.local>
2017-11-13 15:46:34 +0200

    sliders work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,84 @@
 $(document).ready(function () {
 
-  $(window).on('load scroll', function () {
-    var scrolled = $(this).scrollTop();
-    $('#headerVideo').css('transform', 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
-  });
-
-  if ($(window).width() >= 1370){
-    $("body").niceScroll({
-      scrollspeed: 100,
-      zindex:-1
-    });
-  }
-
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    // var headerVideo = $('#headerVideo').height();
-    console.log(scroll, headerVideo);
-    if (scroll >= headerVideo) {
-      $("#videoBottom").addClass("fixed");
-    } else{
-      $("#videoBottom").removeClass("fixed");
+  let slider = $('#capabilitySlider').lightSlider({
+    item: 3,
+    loop: true,
+    autoWidth: false,
+    pager: false,
+    slideMove: 1,
+    slideMargin: 10,
+    adaptiveHeight: true,
+    useCSS: true,
+    controls: false,
+    centerSlide: true,
+    onAfterSlide: function (el) {
+      setTimeout(function(){
+        el.children('li:not(.centerSlide)').css('transform','scale(.85)');
+      },50);
+      el.children('li.centerSlide').removeClass('centerSlide');
+      el.children('li.active').next().addClass('centerSlide');
+    },
+    onSliderLoad: function (el) {
+      setTimeout(function(){
+        el.children('li:not(.centerSlide)').css('transform','scale(.85)');
+      },50);
+      $('#prevSlide').on('click', function(e){
+        e.preventDefault();
+        slider.goToPrevSlide();
+      })
+      $('#nextSlide').on('click', function(e){
+        e.preventDefault();
+        slider.goToNextSlide();
+      })
     }
   });
+  
+  $('#logosSlider').lightSlider({
+    item: 6,
+    slideMove: 1,
+    loop: true,
+    auto: true,
+    pager: false,
+    easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    speed: 1000,
+    pause: 3000,
+    slideMargin: 30,
+    responsive: [
+      {
+        breakpoint:1200,
+        settings: {
+          item:5
+        }
+      },
+      {
+        breakpoint:992,
+        settings: {
+          item:4
+        }
+      },
+      {
+        breakpoint:768,
+        settings: {
+          item:3
+        }
+      },
+      {
+        breakpoint:560,
+        settings: {
+          item:2
+        }
+      }
+    ]
+  });
+
+  $('#whatSaySlider').lightSlider({
+    item: 1,
+    slideMove: 1,
+    loop: false,
+    controls: false,
+    easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    speed: 1000,
+    slideMargin: 0
+  });
+  
 });
